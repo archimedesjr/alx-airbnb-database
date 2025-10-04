a4af44d7f97fffc7e53ad414733c51f61d363543
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-10-04 08:36:26 +0100

    Fixed the errors in the joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,17 +1,18 @@
 -- INNER JOIN to retrieve all bookings and the respective users who made those bookings.
 SELECT *
-FROM bookings
-INNER JOIN users
-  ON bookings.user_id = users.id;
+FROM Booking
+INNER JOIN User
+  ON Booking.user_id = User.user_id;
 
 -- LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
 SELECT *
-FROM properties
-LEFT JOIN reviews
-  ON properties.property_id = reviews.review_id;
+FROM Property
+LEFT JOIN Review
+ON Property.property_id = Review.property_id
+ORDER BY Property.name;
 
 -- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT *
-FROM users
-FULL OUTER JOIN bookings
-  ON properties.property_id = reviews.review_id;
+FROM User
+FULL OUTER JOIN Booking
+  ON User.user_id=Booking.user_id;
