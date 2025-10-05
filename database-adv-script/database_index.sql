4447047e9d62f68a73645c5799447b46e91d1e2e
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-10-05 08:36:11 +0100

    Update database_index.sql with indexing improvements

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -7,13 +7,13 @@ CREATE INDEX idx_property_name ON Property(property_id, name);
 -- Index for fast lookup by booking_id, user_id, property_id
 CREATE INDEX idx_booking_user_property ON Booking(booking_id, user_id, property_id);
 
-EXPLAIN SELECT *
+EXPLAIN ANALYZE SELECT *
 FROM Booking
 INNER JOIN User
   ON Booking.user_id = User.user_id;
 
 
-EXPLAIN SELECT * 
+EXPLAIN ANALYZE SELECT * 
 FROM Property
 WHERE property_id in (
   SELECT property_id 
@@ -22,7 +22,7 @@ WHERE property_id in (
   HAVING AVG(rating) > 4.0
 );
 
-EXPLAIN SELECT *
+EXPLAIN ANALYZE SELECT *
 FROM User
 WHERE (
   SELECT COUNT(*)
